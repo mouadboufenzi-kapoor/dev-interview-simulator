d7a8bcfc669f4b0daebf71e6b4811e52f67b77ad
mouadboufenzi <mouadboufenzi2001@gmail.com>
2026-09-15 15:33:31 +0200

    test: add cat controller test for mock testing

--- dev-interview-simulator-backend/src/test/java/com/interview/simulator/category/CategoryControllerTest.java
+++ dev-interview-simulator-backend/src/test/java/com/interview/simulator/category/CategoryControllerTest.java
@@ -6,7 +6,6 @@ import com.interview.simulator.category.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -22,7 +21,6 @@ class CategoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
     private CategoryService categoryService;
 
     @Test
